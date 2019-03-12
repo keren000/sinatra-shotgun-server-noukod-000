78722d20fbefd6862822f031cea68f71aa2913e7
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-12 14:49:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+<<<<<<< HEAD
   get '/' do
     "Started my server using Shotgun"
   end
 
 end
+=======
+  get '/' do 
+    "Started my server using Shotgun"
+  end
+  
+end
+>>>>>>> 1b9aec34eb090ca74262ddd75722b2dd2b664364
